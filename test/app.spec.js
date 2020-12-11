71e0390c31b81ad2444ea1c9687d5cae3d9aa0a5
Michael Hicks Richardson <michaelhicksrichardson@Michaels-Mac-mini.local>
2020-12-11 10:57:30 -0500

    more test, added route review

--- test/app.spec.js
+++ test/app.spec.js
@@ -3,7 +3,7 @@
 const supertest = require('supertest');
 const app = require('../src/app');
 
-describe.skip('App', () => {
+describe('App', () => {
   it('GET / responds with 200 containing "Hello, world!"', () => {
     return supertest(app)
       .get('/')
@@ -13,6 +13,19 @@ describe.skip('App', () => {
 
 describe(`POST /songs`, () => {
   it(`creates a Song, responding with 201 and the new song`, function() {
+    return supertest(app)
+    .post('/songs')
+    .send({
+      name: 'mike',
+      title: 'airplane',
+      artist: 'memes'
+    })
+    .expect(201)
+  })
+})
+
+describe(`GET /songs`, () => {
+  it(`GET, responding with 201 and the n`, function() {
     return supertest(app)
     .post('/songs')
     .send({
